b6f9112e8a98a9eebc48ec3315b2365e63b0ca5d
Luthando Vico <luthandocele908@gmail.com>
2026-04-11 11:50:19 +0200

    removed trail on url

--- Car-Rental.Api/Program.cs
+++ Car-Rental.Api/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("ReactApp", p =>
-        p.WithOrigins("https://69d9ae3b313dbf2ddef68891--lucky-mermaid-5322af.netlify.app/")
+        p.WithOrigins("https://69d9ae3b313dbf2ddef68891--lucky-mermaid-5322af.netlify.app")
          .AllowAnyHeader()
          .AllowAnyMethod());
 });
