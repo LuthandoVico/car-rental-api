9bf708f46fffe103414f6ac508d1a732e9b6803c
Luthando Vico <luthandocele908@gmail.com>
2026-07-30 23:11:16 +0200

    Updated Program.cs file with updated netlify link

--- Car-Rental.Api/Program.cs
+++ Car-Rental.Api/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("ReactApp", p =>
-        p.WithOrigins("https://lucky-mermaid-5322af.netlify.app")
+        p.WithOrigins("https://eloquent-alfajores-fb1ef3.netlify.app")
          .AllowAnyHeader()
          .AllowAnyMethod());
 });
